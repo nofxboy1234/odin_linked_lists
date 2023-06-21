59553fcd7e87f3cc46911fd5bc4c184957354a26
Dylan Palmboom <nofxboy1234@gmail.com>
2023-06-21 08:17:25 +0200

    Add tests for Node#value

--- lib/node.rb
+++ lib/node.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
 class Node
-  
+  def initialize(value = nil, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+
+  def value
+    @value
+  end
 end
--- spec/node_spec.rb
+++ spec/node_spec.rb
@@ -1,8 +1,23 @@
 require './lib/node'
 
 RSpec.describe Node do
+  
   describe '#value' do
-    it 'returns the value of the node'
+    context 'when value is not set' do
+      subject(:node) { described_class.new }
+
+      it 'returns nil' do
+        expect(node.value).to eq(nil)
+      end
+    end
+    
+    context 'when value is set to 100' do
+      subject(:node) { described_class.new(100) }
+
+      it 'returns 100' do
+        expect(node.value).to eq(100)
+      end
+    end
   end
 
   describe '#next_node' do
